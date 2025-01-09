4513d4b233f6b89f445add0d8f86cfd059e15c20
safwood <naste-f@yandex.ru>
2025-01-09 22:45:02 +0300

    add migrations

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,26 @@ import (
 	"github.com/Safwood/go-server/pkg/handler"
 	"github.com/Safwood/go-server/pkg/repository"
 	"github.com/Safwood/go-server/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main()  {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Ошибка подключения конфиг файла %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handler := handler.NewHandler(services)
 	
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatalf("Ошибка подключения сервера: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
