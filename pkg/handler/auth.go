0a0b65bacfa1cd795b474d2745f5834963030e2b
safwood <naste-f@yandex.ru>
2025-01-13 20:46:38 +0300

    add sign in method

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -27,6 +27,27 @@ func (h *Handler) signUp(c *gin.Context)  {
 	})
 }
 
-func (h *Handler) signIn(с *gin.Context)  {
-	
+type SignInRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+func (h *Handler) signIn(c *gin.Context)  {
+	var input SignInRequest
+
+	if err := c.BindJSON(&input); err != nil {
+		NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Username)
+
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,12 +3,23 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
 
 	todo "github.com/Safwood/go-server"
 	"github.com/Safwood/go-server/pkg/repository"
+	"github.com/dgrijalva/jwt-go"
 )
 
-const salt = "51651cdfvdvf"
+const (
+	salt = "51651cdfvdvf"
+	tokenTTL = 12 * time.Hour
+	signingKey = "qrkjk#4#%35FSFJledeja#4353KSFjH"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -19,13 +30,32 @@ func newAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
-	user.Password = s.generatePasswordHash(user.Password)
+	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
 }
 
-func (s *AuthService) generatePasswordHash(password string) string {
+func generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
-
+	
 	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
+func (s *AuthService) GenerateToken(username string, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	
+	return token.SignedString([]byte(signingKey))
 }
